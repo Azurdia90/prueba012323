a1c6b99c4a8ca30632c4463a6722543c2dffb0c4
Azurdia90 <1983964490101@ingenieria.usac.edu.gt>
2023-01-23 16:42:21 -0600

    Ruta Protegida primera version

---
+++ Controllers/Pruebas.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace prueba012323.Controllers
+{
+    [Route("Pruebas")]
+    public class Pruebas : Controller
+    {
+        public IConfiguration configuration_value;
+
+        public Pruebas(IConfiguration configuration_request)
+        {
+            configuration_value = configuration_request;
+        }
+
+        [HttpGet]
+        [Route("autorizado")]
+        [Authorize]
+        public dynamic prueba_autorizacion()
+        {
+            return new 
+            {
+                success = true,
+			    message = "usuario valido",
+			    result = "autorizado"
+            };
+        }
+    }
+}
